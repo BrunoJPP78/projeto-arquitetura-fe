df5151133107ea7140c7284da6b554122adf5fb3
BrunoJPP78 <brunojpoker@gmail.com>
2024-07-25 16:32:47 -0300

    Finalizando páginas de detalhes

--- projeto-arquitetura-fe/src/components/Header.tsx
+++ projeto-arquitetura-fe/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Header.css';
@@ -12,12 +13,12 @@ const Header: React.FC<HeaderProps> = ({ imageSrc }) => {
   return (
     <header className="header">
       <div className="header-content">
-        <div>
+        <Link to="/">
           <img src={imageSrc} alt="Logo" className="header-image" />
-        </div>
+        </Link>
         <nav className="nav">
           <ul>
-            <li><button onClick={() => navigate('/home')}>Home</button></li>
+            <li><button onClick={() => navigate('/')}>Home</button></li>
             <li><button onClick={() => navigate('/personagens')}>Personagens</button></li>
             <li><button onClick={() => navigate('/episodios')}>Episódios</button></li>
             <li><button onClick={() => navigate('/localizacao')}>Localização</button></li>
--- projeto-arquitetura-fe/src/pages/Episodios/Episodios.tsx
+++ projeto-arquitetura-fe/src/pages/Episodios/Episodios.tsx
@@ -1,7 +1,7 @@
-// Episodios.tsx
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Episodios.css';
+import { useNavigate } from 'react-router-dom';
 
 interface Episode {
   id: number;
@@ -14,6 +14,7 @@ const Episodios: React.FC = () => {
   const [episodes, setEpisodes] = useState<Episode[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get('https://rickandmortyapi.com/api/episode')
@@ -35,12 +36,16 @@ const Episodios: React.FC = () => {
     return <div>{error}</div>;
   }
 
+  const handleEpisodeClick = (id: number) => {
+    navigate(`/episodios/${id}`);
+  };
+
   return (
     <div className="episodes">
       <h2 className="title-name">Episódios</h2>
       <ul className="episode-list">
         {episodes.map(episode => (
-          <li key={episode.id} className="episode-item">
+          <li key={episode.id} className="episode-item" onClick={() => handleEpisodeClick(episode.id)}>
             <h3>{episode.name}</h3>
             <p><b>Episódio:</b> {episode.episode}</p>
             <p><b>Data Exibição:</b> {episode.air_date}</p>
